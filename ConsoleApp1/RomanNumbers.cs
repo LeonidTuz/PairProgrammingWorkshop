a01798424cc090104fe4e6cc6c0dfb808f3cfff8
Kirmakoff <k.makeev@dodobrands.io>
2025-04-11 16:02:52 +0300

    basic parsing

--- ConsoleApp1/RomanNumbers.cs
+++ ConsoleApp1/RomanNumbers.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace ConsoleApp1;
 
 public class RomanNumbers
@@ -13,8 +15,30 @@ public class RomanNumbers
         {'M', 1000}
     };
 
-    public static int RomanNumberToInt(string number)
+    public static int RomanNumberToInt(string romanNumber)
     {
+        // "IX" = 9
+        // "IV" = 4
+        // "III" = 3
         
+        
+        for (int i = 0; i < number.Length; i++)
+        {
+            // символ справа больше символа слева, значит число составное
+            if ((i + 1) < number.Length && (i + 1) > i)
+            {
+                var romanToParse = string.Concat(i, (i + 1));
+                
+            }
+
+            // символ справа такой же 
+            if ((i + 1) < number.Length && (i + 1) = i)
+            {
+                
+            }
+            
+        }
+        
+        int result = 0;
     };
 }
