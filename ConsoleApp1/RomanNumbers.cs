e6d1eddf4688614f0f49e3a91b7c492de98b6617
leonidtuz <l.tuz@dodobrands.io>
2025-04-11 15:51:46 +0300

    add RomanNumbers.cs

---
+++ ConsoleApp1/RomanNumbers.cs
@@ -0,0 +1,20 @@
+namespace ConsoleApp1;
+
+public class RomanNumbers
+{
+    public static Dictionary<char, int> RomanToArabic = new Dictionary<char, int>
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},  
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    public static int RomanNumberToInt(string number)
+    {
+        
+    };
+}
